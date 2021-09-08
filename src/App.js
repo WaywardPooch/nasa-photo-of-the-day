bc865699f6d85afdaf584f174386f54d45676ff8
Lance Turbes <waywardpooch@users.noreply.github.com>
2021-09-08 13:17:24 -0700

    created Header component; added CSS; installed axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
+import { BASE_URL, API_KEY } from "./.env";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+export default function Header(props) {
+  return (
+    <section className="header">
+      <div className="header-content">
+        <h2 className="header-logo">Join the Frontier</h2>
+      </div>
+    </section>
+  );
+}
