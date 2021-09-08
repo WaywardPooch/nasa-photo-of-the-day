85cfc4ece5ccf32b7d78df4064a15355320a918e
Lance Turbes <waywardpooch@users.noreply.github.com>
2021-09-08 15:21:34 -0700

    fetched APOD from NASA API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
-import { BASE_URL, API_KEY } from "./.env";
+import { BASE_URL, API_KEY } from "./constants";
 import Header from "./components/Header";
 import Card from "./components/Card";
 import Footer from "./components/Footer";
 
 function App() {
+  const [apod, setApod] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
+      .then((response) => {
+        setApod(response.data);
+      })
+      .finally(console.log(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`));
+  }, []);
+
   return (
     <div className="App">
       <Header />
-      <Card />
+      <Card apod={apod} />
       <Footer />
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,15 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
+  const { apod } = props;
+
   return (
     <div className="card">
       <div className="card-content">
-        <img className="card-photo" src="https://picsum.photos/600/400" />
+        <img className="card-photo" src={apod.url} />
         <div className="card-label">
-          <h2>Card Title</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra
-            convallis posuere morbi leo urna molestie at elementum. Tortor id
-            aliquet lectus proin nibh nisl condimentum id venenatis.
-          </p>
+          <h2>NASA Photo of the Day</h2>
+          <p></p>
         </div>
       </div>
     </div>
