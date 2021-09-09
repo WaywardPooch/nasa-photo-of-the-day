73c9337a4e1b3a9b7f76db89e8a471d8aa57fb16
Lance Turbes <waywardpooch@users.noreply.github.com>
2021-09-09 16:12:05 -0700

    added search bar styling

--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -2,18 +2,50 @@ import React from "react";
 import styled from "styled-components";
 
 const DateInputArea = styled.form`
+  display: flex;
+  justify-content: center;
   text-align: center;
   margin: 2rem 0;
+
+  button,
+  input {
+    font-size: 2.5rem;
+    font-weight: 400;
+    font-family: "Roboto", sans-serif;
+    text-align: center;
+    border: none;
+  }
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    max-width: 80rem;
+    letter-spacing: 0.1rem;
+    background-color: ${(props) => props.theme.primaryColor};
+    color: ${(props) => props.theme.white};
+    transition: 200ms;
+    padding: 0 2rem;
+
+    &:hover {
+      background-color: ${(props) => props.theme.secondaryColor};
+    }
+  }
+  input {
+    max-width: 32rem;
+    height: 5rem;
+  }
 `;
 
 export default function DateInput(props) {
   const { getUserDateInput, changeDate } = props;
   return (
     <DateInputArea>
-      <label>DATE (YYYY-MM-DD):</label>
-      <br />
-      <input onChange={getUserDateInput} type="text"></input>
-      <button onClick={changeDate}>SUBMIT</button>
+      <input
+        onChange={getUserDateInput}
+        type="text"
+        placeHolder="YYYY-MM-DD"
+      ></input>
+      <button onClick={changeDate}>SEARCH DATE</button>
     </DateInputArea>
   );
 }
